0638dff00a63697d7dbdbfe0544c9adf80fe454d
fareha beig <farehabeig@gmail.com>
2022-10-28 16:42:53 +0500

    fetching query

--- src/components/RQSuperHeroes.page.js
+++ src/components/RQSuperHeroes.page.js
@@ -2,15 +2,17 @@ import { useQuery } from "react-query";
 import axios from "axios";
 export const RQSuperHeroesPage = () => {
     const fetchSuperHeroes = () => {
-        return axios.get("http://localhost:4000/superheroes");
+        return axios.get("http://localhost:4000/superheroe1s");
     };
     // Requires two arguments , one a unique key, second a function that returns a promise
 
-    const { isLoading, data } = useQuery("super-heroes", fetchSuperHeroes);
+    const { isLoading, data, isError, error } = useQuery("super-heroes", fetchSuperHeroes);
 
-    console.log(isLoading, data);
     if (isLoading) {
         return <h2>Loading ...</h2>;
+    }
+    if (isError) {
+        return <p>{error.message}</p>;
     }
     return (
         <>
